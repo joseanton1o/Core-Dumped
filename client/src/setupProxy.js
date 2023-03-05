34bbfef8e6e9eb55e597b3b65970dac98de55865
joseanton1o <joseantonioperezlara@gmail.com>
2023-03-05 22:47:28 +0200

    credit given to proxy setup help

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // Approach to proxying requests changed, as it was not working from the package.json file
+// found this here: https://stackoverflow.com/questions/52605997/when-specified-proxy-in-package-json-must-be-a-string/67335451#67335451
 module.exports = function(app) {
     app.use(
         '/users',
